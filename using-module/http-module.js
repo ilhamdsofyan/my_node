b6a0f8c035e6340d31c7be2b35194789e70f6303
IlhamDSofyan <ilham@docotel.com>
2018-10-22 15:12:46 +0700

    Testing module http dengan load file html

--- using-module/http-module.js
+++ using-module/http-module.js
@@ -1,3 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/html"});
 
+    if (request.url === "/") {
+        response.write("Hell o World");
+        response.end();
+    }
+
+    if (request.url === "/test") {
+        fs.readFile("../test.html", null, (error, file) => {
+            if (error) {
+                response.writeHead(404);
+                response.write("File Not Found");
+            } else {
+                response.write(file);
+            }
+
+            response.end();
+        });
+    }
+});
+
+// server.on("connection", (socket) => {
+//     console.log("New connection...");
+// });
+
+server.listen(3000);
+
+console.log("Listening on port 3000...");
