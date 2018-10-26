c888c94a8f378d912f1e5fd2fd4c72329e03a171
IlhamDSofyan <ilham@docotel.com>
2018-10-26 14:56:59 +0700

    Tambah if-else untuk menghindari error karena  data 2 kali

--- rest-api/index.js
+++ rest-api/index.js
@@ -34,28 +34,29 @@ app.get('/api/customer/:id', function(req, res) {
     if (!single_customer) {
         // 404 bad req
         res.status(404).send(displayMessage("404 <br/> The customer you're looking for is not found. So, get the fuck out of here!!!"));
+    } else {
+        res.send(single_customer);
     }
-
-    res.send(single_customer);
 });
 
 app.post('/api/customer', function(req, res) {
     if (!req.body.name || req.body.name.length < 3) {
         res.status(404).send(displayMessage("Name is required and should be minimum 3 character, obey the rules sunapabic"));
+    } else {
+        // add new data
+        let new_id = customers.length + 1;
+        let customer = {
+            id: new_id,
+            name: req.body.name
+        };
+        customers.push(customer);
+        
+        // let single_customer = customers.find(cust => cust.id === parseInt(new_id));
+        // if (!single_customer) res.status(500).send(displayMessage("500 <br/> The customer you're saved has failed. It's your fault Mutafaka!!!"));
+        
+        res.send(customers);
     }
 
-    // add new data
-    let new_id = customers.length + 1;
-    let customer = {
-        id: new_id,
-        name: req.body.name
-    };
-    customers.push(customer);
-    
-    // let single_customer = customers.find(cust => cust.id === parseInt(new_id));
-    // if (!single_customer) res.status(500).send(displayMessage("500 <br/> The customer you're saved has failed. It's your fault Mutafaka!!!"));
-    
-    res.send(customers);
 });
 
 app.delete('/api/customer/:id', function(req, res){
